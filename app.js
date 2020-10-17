b60ad930367cd8cab8cce53771f9b225df644bf4
lodi palle <lodipalle@gmail.com>
2020-10-17 14:33:04 +1100

    refined h4 class giveaway on index.html and defined constants on app.js

--- app.js
+++ app.js
@@ -25,3 +25,14 @@ const weekdays = [
 const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
+
+let futureDate = new Date(2020, 10, 1, 17, 0, 0);
+console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hrs = futureDate.getHours();
+const mins = futureDate.getMinutes();
+
+giveaway.textContent = `Giveaway ends on ${day}, ${year} at ${hrs}:${mins}:${secs}`;
+
+// Giveaway ends on Sunday, 1 November 2020 at 17:00:00!
