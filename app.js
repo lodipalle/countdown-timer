7109c9183e3707b0a051d8afb4402ff236be9742
lodi palle <lodipalle@gmail.com>
2020-10-18 10:01:18 +1100

    added more functionalities to the countdown timer

--- app.js
+++ app.js
@@ -27,12 +27,21 @@ const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 
 let futureDate = new Date(2020, 10, 1, 17, 0, 0);
-console.log(futureDate);
+// console.log(futureDate);
 
 const year = futureDate.getFullYear();
 const hrs = futureDate.getHours();
 const mins = futureDate.getMinutes();
 
-giveaway.textContent = `Giveaway ends on ${day}, ${year} at ${hrs}:${mins}:${secs}`;
+let month = futureDate.getMonth();
+// console.log(months[month]);
+month = months[month];
+
+const date = futureDate.getDate();
+const weekday = weekdays[futureDate.getDay()];
+// console.log(weekday);
+
+
+giveaway.textContent = `Giveaway ends on ${weekday}, ${date} ${month} ${year} at ${hrs}:${mins}:${secs}`;
 
 // Giveaway ends on Sunday, 1 November 2020 at 17:00:00!
